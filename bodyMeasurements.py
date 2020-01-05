cd833ced4971fa4d83e5c9462e4d840d6bd37470
Anchal Gupta <anchal@caltech.edu>
2020-01-05 00:31:40 -0800

    Removed time delay

--- bodyMeasurements.py
+++ bodyMeasurements.py
@@ -1,7 +1,6 @@
 import gspread
 import numpy as np
 from datetime import timedelta, datetime
-import time
 from oauth2client.service_account import ServiceAccountCredentials
 import matplotlib.pyplot as plt  # For plotting
 import matplotlib.dates as mdates
@@ -155,5 +154,3 @@ lbs.set_title('Body Measurements', fontsize=20)
 
 fig.autofmt_xdate()
 fig.savefig('BodyMeasurementsTimeSeries.pdf', bbox_inches='tight')
-
-time.sleep(6*60)
