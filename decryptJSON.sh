3241dab87ba8d8966907894a9a83f7151fc83ccc
Anchal Gupta <anchal@caltech.edu>
2022-09-03 01:49:32 -0700

    Trying sha256 digest

--- decryptJSON.sh
+++ decryptJSON.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openssl enc -nosalt -aes-256-cbc -d -in bodymeasurements-d291f81a84a8.json.enc -out bodymeasurements-d291f81a84a8.json -base64 -K "$ENCKEY" -iv "$INIT_VECT"
+openssl enc -nosalt -aes-256-cbc -d -md sha256 -in bodymeasurements-d291f81a84a8.json.enc -out bodymeasurements-d291f81a84a8.json -base64 -K "$ENCKEY" -iv "$INIT_VECT"
